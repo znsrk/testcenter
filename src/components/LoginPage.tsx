8edf6aa90c7f893e7816a7d314ee0f322bc4e8c0
znsrk <251311@astanait.edu.kz>
2025-10-17 21:02:20 +0500

    Fixed some lil bs

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { supabase } from '../lib/supabase';
+//import { supabase } from '../lib/supabase';
 
 interface LoginPageProps {
   onNavigate: (page: string) => void;
